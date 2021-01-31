ff03df6efeb5435133ca80834c183a3d1010d75b
Archie Baer <archie@abaer.dev>
2021-01-31 01:27:26 +0000

    Updated hero sizing

--- src/Hero.js
+++ src/Hero.js
@@ -2,7 +2,7 @@ import { Search } from "./Search";
 
 export const Hero = ({ image }) => (
   <div
-    className="w-full h-80 shadow-xl bg-cover bg-center pt-32"
+    className="w-full shadow-xl bg-cover bg-center py-32 px-5"
     style={{ backgroundImage: `url("${image}")` }}
   >
     <div className="mx-auto max-w-xl">
