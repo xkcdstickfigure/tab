e4435ea6b3ffe49d45b33769d332e197fca5ca47
Archie Baer <archie@abaer.dev>
2021-01-31 02:44:29 +0000

    Custom form submission behaviour

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,21 @@
 import { ArrowRight } from "react-feather";
+import { useState } from "react";
 
 export const Search = () => {
+  const [value, setValue] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (value)
+      window.location.href = `https://search.alles.cx/${encodeURIComponent(
+        value
+      )}`;
+  };
+
   return (
     <form
       className="flex shadow-lg overflow-hidden rounded-md"
-      action="https://search.alles.cx"
-      method="GET"
+      onSubmit={submit}
     >
       <input
         className="bg-white py-2 px-5 flex-grow min-w-0"
@@ -13,12 +23,15 @@ export const Search = () => {
         placeholder="Search the web..."
         autoComplete="off"
         autoFocus="on"
+        onChange={(e) => setValue(e.target.value.trim())}
       />
       <button
-        className="flex-shrink-0 bg-white text-gray-600 p-2 hover:bg-primary hover:text-white focus:bg-primary focus:text-white"
+        className={`flex-shrink-0 bg-white ${
+          value ? `text-primary` : `text-gray-600`
+        } p-2 hover:bg-primary hover:text-white focus:bg-primary focus:text-white`}
         type="submit"
       >
-        <ArrowRight size={24} strokeWidth={3} />
+        <ArrowRight size={24} strokeWidth={2.5} />
       </button>
     </form>
   );
