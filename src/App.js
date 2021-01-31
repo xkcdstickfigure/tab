929b65d81a6dac5ad0899244db0bc35b547293e3
Archie Baer <archie@abaer.dev>
2021-01-31 01:54:47 +0000

    Avatar next to search bar

--- src/App.js
+++ src/App.js
@@ -2,5 +2,8 @@ import { Hero } from "./Hero";
 import "./App.css";
 
 export const App = () => (
-  <Hero image="https://images.unsplash.com/photo-1598393412376-51f81f48402f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"></Hero>
+  <Hero
+    userId="bc73a30a-fa43-405c-9d94-ffff4d1268aa"
+    image="https://images.unsplash.com/photo-1598393412376-51f81f48402f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+  />
 );
--- src/Hero.js
+++ src/Hero.js
@@ -1,12 +1,24 @@
 import { Search } from "./Search";
 
-export const Hero = ({ image }) => (
+export const Hero = ({ image, userId }) => (
   <div
     className="w-full shadow-xl bg-cover bg-center py-32 px-5"
     style={{ backgroundImage: `url("${image}")` }}
   >
     <div className="mx-auto max-w-lg">
-      <Search />
+      <div className="relative">
+        <Search />
+        {userId && (
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://account.alles.cx"
+            class="absolute top-0 -right-14 w-10 h-10 rounded-full overflow-hidden shadow-lg"
+          >
+            <img src={`https://avatar.alles.cx/${userId}?size=40`} alt="" />
+          </a>
+        )}
+      </div>
     </div>
   </div>
 );
