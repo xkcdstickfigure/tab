fb1be04228de551eb64fd0cb15bea2addf262f8b
Archie Baer <archie@abaer.dev>
2021-01-31 02:13:01 +0000

    autocomplete => autoComplete

--- src/Search.js
+++ src/Search.js
@@ -11,7 +11,7 @@ export const Search = () => {
         className="bg-white py-2 px-5 flex-grow min-w-0"
         name="q"
         placeholder="Search the web..."
-        autocomplete="off"
+        autoComplete="off"
       />
       <button
         className="flex-shrink-0 bg-white text-gray-600 p-2 hover:bg-primary hover:text-white focus:bg-primary focus:text-white"
