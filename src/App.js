c655afc540b7f8b34e32dae66a16d90ccc5d7c7e
Archie Baer <archie@abaer.dev>
2021-02-01 22:06:06 +0000

    Fetches data from homepage endpoint

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { get as getCookie } from "es-cookie";
 import "./App.css";
 
 import { Hero } from "./Hero";
 import { Card } from "./Card";
 
-export const App = () => (
-  <div className="space-y-10">
-    <Hero
-      userId="bc73a30a-fa43-405c-9d94-ffff4d1268aa"
-      image="https://images.unsplash.com/photo-1598393412376-51f81f48402f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-    />
-    <div className="mx-auto max-w-3xl grid gap-3 grid-cols-5">
-      <Card width={3} height={2}></Card>
-      <Card width={2}></Card>
-      <Card width={2} height={2}></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-    </div>
-  </div>
-);
+export const App = () => {
+  const [data, setData] = useState(null);
+  const fetchData = async () => {
+    try {
+      const api = getCookie("protium-api") || "https://protium.alles.cx/c";
+      const token = getCookie("protium-token");
+      setData({
+        api,
+        token,
+        ...(await axios.get(`${api}/hp`, { headers: { Authorization: token } }))
+          .data,
+      });
+    } catch (err) {}
+  };
+  useEffect(() => {
+    fetchData();
+    const i = setInterval(fetchData, 2500);
+    return () => clearInterval(i);
+  }, []);
+
+  return (
+    data && (
+      <div className="space-y-10">
+        <Hero userId={data.user.id} image={data.background} />
+        <div className="mx-auto max-w-3xl grid gap-3 grid-cols-5">
+          <Card width={3} height={2}></Card>
+          <Card width={2}></Card>
+          <Card width={2} height={2}></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
+      </div>
+    )
+  );
+};
