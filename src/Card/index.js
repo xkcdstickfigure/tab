46269c62f9b70ba684b316ec523f3e73b503e490
Archie Baer <archie@abaer.dev>
2021-02-02 13:05:19 +0000

    Card Styling

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,7 +1,13 @@
-export const Card = ({ children, width = 1, height = 1, className = "" }) => (
+export const Card = ({
+  children,
+  width = 1,
+  height = 1,
+  className = "",
+  style,
+}) => (
   <div
     className={`bg-white rounded-md shadow-md col-span-${width} row-span-${height} ${className}`}
-    style={{ height: `${height * 8 + (height - 1) * 0.75}rem` }}
+    style={{ height: `${height * 8 + (height - 1) * 0.75}rem`, ...style }}
   >
     {children}
   </div>
