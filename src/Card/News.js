d9c756e405456a0267a0acbaeb2085588e3de937
Archie Baer <archie@abaer.dev>
2021-02-03 00:22:00 +0000

    News item arrows

--- src/Card/News.js
+++ src/Card/News.js
@@ -1,28 +1,57 @@
 import { Card } from ".";
 import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "react-feather";
 
 export const NewsCard = ({ items, ...props }) => {
+  const [showArrows, setArrows] = useState(false);
+
   const [i, setI] = useState(0);
   const item = items[i];
 
+  const next = () => setI(i >= items.length - 1 ? 0 : i + 1);
+  const prev = () => setI(i <= 0 ? items.length - 1 : i - 1);
+
   return (
-    <Card className="p-8 space-y-5 overflow-hidden" {...props}>
-      <div className="space-y-2">
-        <h2 className="uppercase text-sm text-gray-600 font-semibold">
-          {item.topic}
-        </h2>
-        <h1 className="text-3xl font-bold">{item.title}</h1>
+    <Card
+      className="flex"
+      onMouseEnter={() => setArrows(true)}
+      onMouseLeave={() => setArrows(false)}
+      {...props}
+    >
+      <div
+        className={`w-8 flex-shrink-0 flex flex-col justify-center duration-100 ${
+          showArrows ? `opacity-100` : `opacity-0`
+        }`}
+        onClick={next}
+      >
+        <ChevronLeft className="mx-auto text-gray-600" size={30} />
       </div>
-      <div className="flex space-x-2">
-        {item.sites.map((site) => (
-          <a href={site.url} title={site.name}>
-            <img
-              src={site.icon}
-              alt={site.name}
-              className="w-8 h-8 rounded-sm"
-            />
-          </a>
-        ))}
+      <div className="px-4 py-8 flex-grow space-y-5">
+        <div className="space-y-2">
+          <h2 className="uppercase text-sm text-gray-600 font-semibold">
+            {item.topic}
+          </h2>
+          <h1 className="text-3xl font-bold">{item.title}</h1>
+        </div>
+        <div className="flex space-x-2">
+          {item.sites.map((site) => (
+            <a href={site.url} title={site.name}>
+              <img
+                src={site.icon}
+                alt={site.name}
+                className="w-8 h-8 rounded-sm"
+              />
+            </a>
+          ))}
+        </div>
+      </div>
+      <div
+        className={`w-8 flex-shrink-0 flex flex-col justify-center duration-100 ${
+          showArrows ? `opacity-100` : `opacity-0`
+        }`}
+        onClick={prev}
+      >
+        <ChevronRight className="mx-auto text-gray-600" size={30} />
       </div>
     </Card>
   );
--- src/Card/index.js
+++ src/Card/index.js
@@ -4,10 +4,12 @@ export const Card = ({
   height = 1,
   className = "",
   style,
+  ...props
 }) => (
   <div
-    className={`bg-white rounded-md shadow-md col-span-${width} row-span-${height} ${className}`}
+    className={`bg-white rounded-md overflow-hidden shadow-md col-span-${width} row-span-${height} ${className}`}
     style={{ height: `${height * 8 + (height - 1) * 0.75}rem`, ...style }}
+    {...props}
   >
     {children}
   </div>
