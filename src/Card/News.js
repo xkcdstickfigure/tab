13913e7b6bd53a503c59abf39779d8c8cd9ade84
Archie Baer <archie@abaer.dev>
2021-02-03 00:51:30 +0000

    Automatically advances news item every 5 seconds

--- src/Card/News.js
+++ src/Card/News.js
@@ -1,5 +1,5 @@
 import { Card } from ".";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ChevronLeft, ChevronRight } from "react-feather";
 
 export const NewsCard = ({ items, ...props }) => {
@@ -10,6 +10,13 @@ export const NewsCard = ({ items, ...props }) => {
 
   const next = () => setI(i >= items.length - 1 ? 0 : i + 1);
   const prev = () => setI(i <= 0 ? items.length - 1 : i - 1);
+  const n = useCallback(next, [i, items.length]);
+
+  useEffect(() => {
+    if (i >= items.length) setI(0);
+    const int = setInterval(n, 5000);
+    return () => clearInterval(int);
+  }, [n, i, items.length]);
 
   return (
     <Card
@@ -27,23 +34,27 @@ export const NewsCard = ({ items, ...props }) => {
         <ChevronLeft className="mx-auto text-gray-600" size={30} />
       </div>
       <div className="px-2 py-8 flex-grow space-y-5 overflow-hidden">
-        <div className="space-y-2">
-          <h2 className="uppercase text-sm text-gray-600 font-semibold">
-            {item.topic}
-          </h2>
-          <h1 className="text-3xl font-bold">{item.title}</h1>
-        </div>
-        <div className="flex space-x-2">
-          {item.sites.map((site) => (
-            <a href={site.url} title={site.name}>
-              <img
-                src={site.icon}
-                alt={site.name}
-                className="w-8 h-8 rounded-sm"
-              />
-            </a>
-          ))}
-        </div>
+        {item && (
+          <>
+            <div className="space-y-2">
+              <h2 className="uppercase text-sm text-gray-600 font-semibold">
+                {item.topic}
+              </h2>
+              <h1 className="text-3xl font-bold">{item.title}</h1>
+            </div>
+            <div className="flex space-x-2">
+              {item.sites.map((site, s) => (
+                <a key={s} href={site.url} title={site.name}>
+                  <img
+                    src={site.icon}
+                    alt={site.name}
+                    className="w-8 h-8 rounded-sm"
+                  />
+                </a>
+              ))}
+            </div>
+          </>
+        )}
       </div>
       <div
         className={`w-8 flex-shrink-0 flex flex-col justify-center duration-100 ${
