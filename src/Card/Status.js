3a59c8119c9c12c7c37ec7e5497e9d17cd14079b
Archie Baer <archie@abaer.dev>
2021-02-16 02:44:20 +0000

    Improved status card

--- src/Card/Status.js
+++ src/Card/Status.js
@@ -4,19 +4,17 @@ import axios from "axios";
 
 export const StatusCard = ({ api, token, status, ...props }) => {
   const [value, setValue] = useState(status || "");
-  const [highlight, setHighlight] = useState(true);
 
   const submit = (e) => {
     e.preventDefault();
-    setHighlight(false);
-    if (highlight)
-      axios
-        .post(
-          `${api}/status`,
-          { content: value },
-          { headers: { Authorization: token } }
-        )
-        .catch(() => {});
+    e.target.status.blur();
+    axios
+      .post(
+        `${api}/status`,
+        { content: value },
+        { headers: { Authorization: token } }
+      )
+      .catch(() => {});
   };
 
   return (
@@ -26,17 +24,12 @@ export const StatusCard = ({ api, token, status, ...props }) => {
       </h1>
       <form onSubmit={submit}>
         <input
+          name="status"
           defaultValue={status}
           placeholder="Being awesome!"
           maxLength={100}
-          onChange={(e) => {
-            setValue(e.target.value);
-            setHighlight(true);
-          }}
-          onFocus={() => setHighlight(true)}
-          className={`w-full p-2 outline-none duration-100 border-b-2 ${
-            highlight ? `focus:border-primary` : ``
-          }`}
+          onChange={(e) => setValue(e.target.value)}
+          className="w-full p-2 outline-none duration-100 border-b-2 focus:border-primary"
         />
       </form>
     </Card>
