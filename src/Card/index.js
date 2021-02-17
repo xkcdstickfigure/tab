c85b75db19fcab98c2c22d0dbed9d6d137bdb425
Archie Baer <archie@abaer.dev>
2021-02-17 17:05:00 +0000

    Added commented class names to avoid purgecss stripping them out

--- src/Card/index.js
+++ src/Card/index.js
@@ -14,3 +14,15 @@ export const Card = ({
     {children}
   </div>
 );
+
+// row-span-1
+// row-span-2
+// row-span-3
+// row-span-4
+// row-span-5
+
+// col-span-1
+// col-span-2
+// col-span-3
+// col-span-4
+// col-span-5
